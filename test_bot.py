8d8db13ec4425c53522a474a8f9f827ed148a2f8
Wacky52 <wackyislucky@gmail.com>
2025-01-17 15:10:40 +0500

    Update test_bot.py
    version3.0

--- test_bot.py
+++ test_bot.py
@@ -1,16 +1,4 @@
 <!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>3D Portfolio Website HTML CSS and Javascript | Codehal</title>
-    <link rel="stylesheet" href="style.css">
-    <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4./css/boxicons.min.css">
-</head>
-
-<!DOCTYPE html>
 <html>
     <head>
         <title>wacky</title>
@@ -39,3 +27,5 @@
         </a>
     </body>
 </html>
+
+
